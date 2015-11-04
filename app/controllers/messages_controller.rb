94b9dbf9fffbdd5aa7738bf37fe01de2d8b1f8a6
z001_001 <z001-001@drmscp.net>
2015-11-04 12:34:54 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
   def index
+    # Messageをすべて取得する。
+    @messages = Message.all
   end
+  
+  ## ここから
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path, notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
 end
