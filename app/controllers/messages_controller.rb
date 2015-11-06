be90447a490dccfc628246fb88a71143f024c425
z001_001 <z001-001@drmscp.net>
2015-11-06 13:01:51 +0000

    Challenge 2: Adding a new column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,7 +40,7 @@ class MessagesController < ApplicationController
     
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   ## ここまで
   
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20 }, presence: true
     # 内容は必須入力カツ2文字以上30文字以下
     validates :body, length: { minimum: 2, maximum: 30 }, presence: true
+    # 年齢は0以上の数値
+    validates :age, numericality: { only_integer: true,
+        greater_than_or_equal_to: 0, less_than: 256 },
+        presence: true
 end
